85ba58b7a7d1194f90daf0cd46329db4ccd2085e
Mario Prieto <mprieto@csmm.es>
2023-04-11 16:33:34 +0200

    Backend actualizado

--- backend/csmm_project/csmm_project/settings.py
+++ backend/csmm_project/csmm_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'csmm_app.apps.CsmmAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
